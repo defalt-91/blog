5089aec73a1faf80def03068f3733e57ef065a01
defalt91 <armansoltanian@gmail.com>
2021-04-22 16:27:32 +0430

    twovue

--- survey/vue/src/components/NewSurvey.vue
+++ survey/vue/src/components/NewSurvey.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="col-6">
-    <button type="button" class="btn btn-sm btn-outline-info" data-toggle="modal" data-target="#exampleModal">
-        Add Survey
-    </button></div>
+        <button type="button" class="btn btn-sm btn-outline-info" data-toggle="modal" data-target="#exampleModal">
+            Add Survey
+        </button></div>
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -11,7 +11,8 @@
                     <h5 class="modal-title" id="exampleModalLabel">Write your survey question</h5> <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span> </button>
                 </div>
-                <div class="modal-body"> <input type="text" name="question" v-model='question'>
+                <div class="modal-body">
+                    <input type="text" name="question" v-model='question'>
                     <h1>{{question}}</h1>
                 </div>
                 <div class="modal-footer"> <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> <button type="button" @click="addsurvey" class="btn btn-primary">Save
@@ -38,7 +39,7 @@ export default {
                 method: 'POST',
                 url: 'https://arman-blog.herokuapp.com/api/v2/survey/',
                 data: {
-                    question: 'asdasd',
+                    question: this.question,
                 }
             })
         },
